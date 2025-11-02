5d8a62c942c57dc38ea2be7ac96016637ca75a6e
victor-a-win <victoradiwinata@gmail.com>
2025-11-03 01:14:00 +0800

    fix(deployment_vercel): add dynamic exports to statistics  page of eo dashboard

--- web/src/pages/eo-dashboard-page/sections/statistics-section/index.tsx
+++ web/src/pages/eo-dashboard-page/sections/statistics-section/index.tsx
@@ -1,5 +1,8 @@
 import SimpleBarChart from "@/components/basic-statistics/basic-statistics";
 
+// Add this to disable static generation
+export const dynamic = 'force-dynamic';
+
 export default function BasicStatistics() {
     return (
       <div>
